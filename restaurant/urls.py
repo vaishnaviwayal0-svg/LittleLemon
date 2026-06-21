640b6d7eb12885d98f79b5f6258b406cac6ca03d
Vaishnavi <Vaishnaviwayal0@gmail.com>
2026-06-21 10:44:39 +0530

    Step 10 :Completed static routes with logo image

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin 
-from django.urls import path 
-from .views import sayHello 
-  
-urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def sayHello(request):
- return HttpResponse('Hello World')
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
